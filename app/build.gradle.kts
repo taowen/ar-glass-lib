486834dba778ca4439a3fd8a5a8afe62e5adeb00
Tao Wen <taowen@gmail.com>
2026-07-27 22:01:37 +0800

    Release ar-glass-check 1.0.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,8 +9,8 @@ android {
         applicationId = "com.taowen.arglass.demo"
         minSdk = 26
         targetSdk = 36
-        versionCode = 10008
-        versionName = "1.0.8"
+        versionCode = 10009
+        versionName = "1.0.9"
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
